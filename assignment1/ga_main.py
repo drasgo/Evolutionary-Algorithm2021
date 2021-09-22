bf18e345fb47fc7b88e3b37917748271a53b99bd
marcel_s <marcel.stelte@gmail.com>
2021-09-23 00:04:59 +0200

    GA classes created

---
+++ assignment1/ga_main.py
@@ -0,0 +1,14 @@
+from assignment1.ga_controller import player_controller
+from evoman.environment import Environment
+
+n_hidden_neurons = 20
+
+experiment_name = "ga_specialist"
+
+env = Environment(experiment_name=experiment_name,
+                  enemies=[2],
+                  playermode="ai",
+                  player_controller=player_controller(n_hidden_neurons),
+                  enemymode="static",
+                  level=2,
+                  speed="fastest")
