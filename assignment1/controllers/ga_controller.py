a787de0a09674a5165bc3f8984319b1bad1c57c8
marcel_s <marcel.stelte@gmail.com>
2021-09-23 21:03:06 +0200

    Moved files to the correct folder

--- assignment1/controllers/ga_controller.py
+++ assignment1/controllers/ga_controller.py
@@ -2,7 +2,7 @@
 from evoman.controller import Controller
 
 
-class player_controller(Controller):
+class ga_controller(Controller):
 	def __init__(self, _n_hidden):
 		# Number of hidden neurons
 		self.n_hidden = [_n_hidden]
--- assignment1/ga_main.py
+++ assignment1/ga_main.py
@@ -1,5 +1,7 @@
-from ga_controller import player_controller
-from evoman.environment import Environment
+import os
+
+from assignment1.controllers.ga_controller import ga_controller as controller
+from assignment1.environment import Environment
 
 n_hidden_neurons = 20
 
@@ -8,7 +10,12 @@ experiment_name = "ga_specialist"
 env = Environment(experiment_name=experiment_name,
                   enemies=[2],
                   playermode="ai",
-                  player_controller=player_controller(n_hidden_neurons),
+                  player_controller=controller(n_hidden_neurons),
                   enemymode="static",
                   level=2,
                   speed="fastest")
+
+# choose this for not using visuals and thus making experiments faster
+headless = True
+if headless:
+    os.environ["SDL_VIDEODRIVER"] = "dummy"
